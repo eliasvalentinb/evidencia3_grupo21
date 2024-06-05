7fdc9b8981eef755dcb753d8b75653c682f0f003
Elias Valdez <elias.ol333@gmail.com>
2024-06-05 08:44:29 -0300

    se corrigen algunos errores y ahora funciona correctamente

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ En este archivo definiremos el menú de opciones.
 """
 
 from modulo_opciones import organizar_partido, buscar_partido
+import time
 
 def menu_principal():
     print("¡Bienvenido a la aplicación!")
@@ -10,6 +11,7 @@ def menu_principal():
     print("1. Organizar un partido")
     print("2. Unirse a un partido.")
     print("3. Salir")
+    time.sleep(0.5)
 
 def main():
     while True:
@@ -18,13 +20,16 @@ def main():
 
         if opcion == "1":
             organizar_partido()
+            break
         elif opcion == "2":
             buscar_partido()
+            break
         elif opcion == "3":
             print("Saliendo...")
             break
         else:
             print("Opción no válida. Intenta nuevamente.")
+            time.sleep(0.5)
 
-if __name__ == "___main___":
+if __name__ == "__main__":
     main()
