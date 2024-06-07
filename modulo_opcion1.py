059934e0c19cef8e28747f7e5d2fd78b42fe2b5c
Elias Valdez <elias.ol333@gmail.com>
2024-06-06 22:04:38 -0300

    se eliminan archivos para agregar otros actualizados

--- modulo_opcion1.py
+++
@@ -1,6 +0,0 @@
-""" 
-En este archivo definiremos las funciones a utilizar en el archivo 'index.py'
-"""
-
-def organizar_partido():
-    print("Has seleccionado la opción de organizar un partido.")
--- modulo_opcion2.py
+++
@@ -1,3 +0,0 @@
-
-def buscar_partido():
-    print("Has seleccionado la opción de buscar un partido en tu zona.")    
