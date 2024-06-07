e0098829a4c58143c700de9142922d58d878c14f
Elias Valdez <elias.ol333@gmail.com>
2024-06-07 15:21:20 -0300

    se corrige error de ortografía

--- tipo_jugador.py
+++ tipo_jugador.py
@@ -1,5 +1,5 @@
 # Función para elegir tipo de USUARIO
 def tipo_usuario():
-    print("1. Para ingresar com JUGADOR")
+    print("1. Para ingresar como JUGADOR")
     print("2. Para ingresar como INVITADO.")
 
