a6d33dc77075f0760b3250a9fd0c6897c67aa0e6
Elias Valdez <elias.ol333@gmail.com>
2024-06-07 14:48:47 -0300

    creamos una función que de opciones de tipo de usuario

---
+++ tipo_jugador.py
@@ -0,0 +1,16 @@
+# Función para opciones de JUGADOR
+def tipo_usuario():
+    print("1. Para ingresar com JUGADOR")
+    print("2. Para ingresar como INVITADO.")
+
+def organizar_partido():
+    print("Has seleccionado la opción de organizar un partido nuevo.")
+
+def buscar_partido():
+    print("Has seleccionado la opción de buscar partidos en tu zona.")
+
+def modificar_datos():
+    print ("Has seleccionado la opción de MODIFICAR DATOS.")
+
+def eliminar_cuenta():
+    print ("Has seleccionado la opción de ELIMINAR CUENTA.")
